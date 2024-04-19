9f34fb6c9bf29df7f86146b3986585320a4f86e5
user21 <dementev.ru55@gmail.com>
2024-04-19 13:20:54 +0500

    Revert "3.3. Фиксация изменений на github[#243564]"
    This reverts commit 8d9899b233e11c3658f2a291def846b39e2e8adc.

--- src/main/java/ru/job4j/Studentinfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class Studentinfo {
-    public static void main(String[] args){
-        System.out.println("Dmitry Dementev");
-        System.out.println("30.08.2024");
-    }
-}
