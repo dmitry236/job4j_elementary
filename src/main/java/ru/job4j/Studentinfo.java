8d9899b233e11c3658f2a291def846b39e2e8adc
user21 <dementev.ru55@gmail.com>
2024-04-19 13:17:40 +0500

    3.3. Фиксация изменений на github[#243564]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args){
+        System.out.println("Dmitry Dementev");
+        System.out.println("30.08.2024");
+    }
+}
